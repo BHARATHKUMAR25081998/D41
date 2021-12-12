bb9a1e7ffbd8a6148335eb9edaf79981c6fd259f
BHARATHKUMAR25081998 <87696403+BHARATHKUMAR25081998@users.noreply.github.com>
2021-12-12 17:17:15 +0530

    Add files via upload

---
+++ nodeTest.js
@@ -0,0 +1,25 @@
+var http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  let out = "";
+  out += "<p>"
+  fs.readdir("./../../Day_40/PetShopDBDesign/", (err, files) => { 
+  if (err) 
+    console.log(err); 
+  else { 
+    console.log("\nCurrent directory filenames:"); 
+    files.forEach(file => { 
+      console.log(file); 
+      out += "\n" + file;
+    })  
+  } 
+}) 
+  out += "</p>";
+  console.log(out)
+  res.write("out");
+  res.end();
+}).listen(8080);
+
